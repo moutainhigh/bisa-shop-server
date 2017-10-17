139817af84bcbcc2c20f4af8ae3315851e448a24
youge2794 <youge2794@163.com>
2017-10-17 13:53:51 +0800

    解决启动的一些BUG

--- health-shop-controller/src/main/java/com/bisa/hkshop/wqc/filter/SystemContextFilter.java
+++ health-shop-controller/src/main/java/com/bisa/hkshop/wqc/filter/SystemContextFilter.java
@@ -1,31 +1,25 @@
 package com.bisa.hkshop.wqc.filter;
-
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
+import org.apache.shiro.web.filter.PathMatchingFilter;
 import com.bisa.health.model.SystemContext;
 
-
-
-
-public class SystemContextFilter implements Filter{
-	private Integer pageSize;
-
-	public void destroy() {
+public class SystemContextFilter extends PathMatchingFilter{
+	
+	private Integer pageSizeDefalut=6;
+	private Integer pageSize=pageSizeDefalut;
+	@Override
+	protected boolean onPreHandle(ServletRequest req, ServletResponse response, Object mappedValue)
+			throws Exception {
 		
-	}
-
-	public void doFilter(ServletRequest req, ServletResponse resp,
-			FilterChain chain) throws IOException, ServletException {
-		//System.out.println("=======guolv");
+		
 		Integer offset = 0;
+		try {
+			pageSize = Integer.parseInt(req.getParameter("pageSize"));
+		} catch (NumberFormatException e) {
+			pageSize = pageSizeDefalut;
+		}
 		try {
 			offset = Integer.parseInt(req.getParameter("pager.offset"));
 		} catch (NumberFormatException e) {}
@@ -35,25 +29,19 @@ public class SystemContextFilter implements Filter{
 			SystemContext.setPageOffset(offset);
 			SystemContext.setPageSize(pageSize);
 			SystemContext.setRealPath(((HttpServletRequest)req).getSession().getServletContext().getRealPath("/"));
-			chain.doFilter(req,resp);
-		} finally {
+			return true;
+		} catch (Exception e) {
+
 			SystemContext.removeOrder();
 			SystemContext.removeSort();
 			SystemContext.removePageOffset();
 			SystemContext.removePageSize();
 			SystemContext.removeRealPath();
+			return true;
+		
 		}
+	
 	}
 
-	public void init(FilterConfig cfg) throws ServletException {
-		//System.out.println("=======guolv");
-		try {
-			pageSize = Integer.parseInt(cfg.getInitParameter("pageSize"));
-			//System.out.println("============"+pageSize);
-		} catch (NumberFormatException e) {
-			pageSize = 2;
-			//System.out.println("开始============"+pageSize);
-		}
-	}
 
 }
