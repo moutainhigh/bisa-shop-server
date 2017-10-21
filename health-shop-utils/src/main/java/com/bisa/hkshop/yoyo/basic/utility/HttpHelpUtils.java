51a473f73cf333cb9d4cf49956dbb1085d61e3b9
youge2794 <youge2794@163.com>
2017-10-21 16:49:34 +0800

    同不会话修改BUG

--- health-shop-utils/src/main/java/com/bisa/hkshop/yoyo/basic/utility/HttpHelpUtils.java
+++ health-shop-utils/src/main/java/com/bisa/hkshop/yoyo/basic/utility/HttpHelpUtils.java
@@ -1,7 +1,12 @@
 package com.bisa.hkshop.yoyo.basic.utility;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.util.StringUtils;
 
 public class HttpHelpUtils {
 	
@@ -13,5 +18,15 @@ public class HttpHelpUtils {
 			  }
 		  }
 		  return null;
+	}
+
+	public static String[] getIP(String url) {
+			if(StringUtils.isEmpty(url)){
+				return null;
+			}
+			String[] urlAtr=new String[2];
+			Pattern p = Pattern.compile("(http://|https://)?([^/]*)",Pattern.CASE_INSENSITIVE);
+		    Matcher m = p.matcher(url);
+		    return m.find()?m.group(2).split("\\:"):null;
 	}
 }
