5f1dcef0296c5ce9c0fcd2bf8cef10076189989c
youge2794 <youge2794@163.com>
2017-10-23 15:58:06 +0800

    登入BUG

--- health-shop-controller/src/main/java/com/bisa/hkshop/yoyo/controller/SessionController.java
+++ health-shop-controller/src/main/java/com/bisa/hkshop/yoyo/controller/SessionController.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bisa.health.model.User;
 
 /**
  * 测试更新会话
@@ -23,7 +26,8 @@ public class SessionController {
 	
 	@RequestMapping(value="/user/testlog",method=RequestMethod.GET)
 	public @ResponseBody String updateSession(HttpServletRequest request, HttpServletResponse response,Model model){
-		logger.debug("TEST");
+		User user=(User)SecurityUtils.getSubject().getSession().getAttribute("user");
+		System.out.println(user.getUser_guid());
 		return "success";
 		
 	}
